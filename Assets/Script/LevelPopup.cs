7371eedc2001ef521365a2ac42090673521a073c
chimpuchim <96745654+chimpuchim@users.noreply.github.com>
2022-11-15 17:07:29 +0700

    +add complete scene

---
+++ Assets/Script/LevelPopup.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelPopup : MonoBehaviour
+{
+
+    //1294.241 / 6 = số 1 button chiếm grid
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(2377.388f / -1188.694f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
