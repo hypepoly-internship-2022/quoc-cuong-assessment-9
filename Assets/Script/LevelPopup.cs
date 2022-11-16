ddd8f7ad0b5461643fcec61bbf72316592faa334
chimpuchim <96745654+chimpuchim@users.noreply.github.com>
2022-11-16 16:21:44 +0700

    +add scroll scene and spawn prefab

--- Assets/Script/LevelPopup.cs
+++ Assets/Script/LevelPopup.cs
@@ -5,17 +5,38 @@ using UnityEngine;
 public class LevelPopup : MonoBehaviour
 {
 
-    //1294.241 / 6 = số 1 button chiếm grid
+    [SerializeField] private GameObject btnLevelPrefab;
+    [SerializeField] private GameObject gridLayout;
+    [SerializeField] private int levelUWant;
+    [SerializeField] private string[] textUWant;
+
+    //215.7 = số 1 button chiếm grid
 
     // Start is called before the first frame update
     void Start()
-    {
-        Debug.Log(2377.388f / -1188.694f);
+    {   
+        SpawnSizeGrid(levelUWant);
+        spawnLevelBtn(levelUWant);
+    }
+
+    void spawnLevelBtn(int idx){
+        for(int i=0; i<idx; i++){
+            GameObject game = Instantiate(btnLevelPrefab, new Vector3(0,0,0), Quaternion.identity);
+            game.transform.SetParent(gridLayout.transform);
+            game.name = (i+1).ToString();
+        }   
+    }
+
+    void SpawnSizeGrid(int idx){
+        RectTransform rt = gridLayout.GetComponent<RectTransform>();
+        if(idx > 5){
+            rt.position = new Vector3(540, 219f * idx / -2, 0);
+            rt.sizeDelta = new Vector2(656.29f, 220.7f * idx);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
