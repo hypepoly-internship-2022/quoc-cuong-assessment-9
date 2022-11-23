370f58ac4ccf7e7e05704ce1d514da83ed2e07e6
chimpuchim <96745654+chimpuchim@users.noreply.github.com>
2022-11-23 14:59:23 +0700

    *fix bugs UI

--- Assets/Script/LevelPopup.cs
+++ Assets/Script/LevelPopup.cs
@@ -23,6 +23,7 @@ public class LevelPopup : MonoBehaviour
         for(int i=0; i<idx; i++){
             GameObject btn = Instantiate(btnLevelPrefab, new Vector3(0,0,0), Quaternion.identity);
             btn.transform.SetParent(gridLayout.transform);
+            btn.transform.localScale = new Vector3(1,1,0);
             btn.name = (i+1).ToString();
             for(int j=0; j<btn.transform.childCount; j++){
                 Transform curChild = btn.transform.GetChild(j);
@@ -32,19 +33,20 @@ public class LevelPopup : MonoBehaviour
                 }
             }
         }   
+
     }
 
     void SpawnSizeGrid(int idx){
         RectTransform rt = gridLayout.GetComponent<RectTransform>();
         if(idx > 5){
-            rt.position = new Vector3(540, 219f * idx / -2, 0);
-            rt.sizeDelta = new Vector2(656.29f, 220.7f * idx);
+            rt.sizeDelta = new Vector2(353.13f, 136f * idx);
+            rt.localPosition = new Vector3(0, 219f * idx / -2, 0);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
